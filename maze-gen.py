f65584dfbf557b9a40b3c1aea354b6d321f212c3
dapeef <dapeef@users.noreply.github.com>
2020-07-06 00:54:22 +0100

    Create and implement all in maze-gen

---
+++ maze-gen.py
@@ -0,0 +1,63 @@
+import random
+
+
+def generate_maze(size_x, size_y):
+    nodes = [Node(i + j * size_x, (i, j)) for j in range(size_y)
+             for i in range(size_x)]
+
+    walls = [
+        Wall("r", (i, j), True) for j in range(size_y)
+        for i in range(size_x - 1)
+    ] + [
+        Wall("b", (i, j), True) for j in range(size_y - 1)
+        for i in range(size_x)
+    ]
+
+    random.shuffle(walls)
+
+    for wall in walls:
+        if wall.orientation == "r":
+            parent_nodes = [nodes[wall.position[0] + size_x * wall.position[1]],
+                            nodes[wall.position[0] + 1 + size_x * wall.position[1]]]
+
+        else:
+            parent_nodes = [nodes[wall.position[0] + size_x * wall.position[1]],
+                            nodes[wall.position[0] + size_x * (wall.position[1] + 1)]]
+
+        if parent_nodes[0].group_id != parent_nodes[1].group_id:
+            wall.enabled = False
+
+            dead_group_id = parent_nodes[1].group_id
+
+            for node in nodes:
+                if node.group_id == dead_group_id:
+                    node.group_id = parent_nodes[0].group_id
+
+    right_walls = [[True for i in range(size_x - 1)] for i in range(size_y)]
+    bottom_walls = [[True for i in range(size_x)] for i in range(size_y - 1)]
+
+    for wall in walls:
+        if wall.orientation == "r":
+            right_walls[wall.position[1]][wall.position[0]] = wall.enabled
+
+        if wall.orientation == "b":
+            bottom_walls[wall.position[1]][wall.position[0]] = wall.enabled
+
+    return (right_walls, bottom_walls)
+
+
+class Node:
+    def __init__(self, group_id, position):
+        self.group_id = group_id
+        self.position = position
+
+
+class Wall:
+    def __init__(self, orientation, position, enabled):
+        self.orientation = orientation
+        self.position = position
+        self.enabled = enabled
+
+
+if __name__ == "__main__":
+    print(generate_maze(4, 4))
