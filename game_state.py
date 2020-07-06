6327a3f993db7ab40f1eceadec9c7570cd3fa355
Kneasle <kneasle@gmail.com>
2020-07-06 22:59:23 +0100

    Rename 'sid' to 'tag' in GameState

--- game_state.py
+++ game_state.py
@@ -7,17 +7,17 @@ class GameState:
     def add_tank(self, x, y, r, colour, name, sid):
         self._tanks[sid] = Tank(x, y, r, colour, name, sid)
 
-    def update_tank(self, sid, tank_json):
-        self._tanks[sid].update_from_json(tank_json)
+    def update_tank(self, tag, tank_json):
+        self._tanks[tag].update_from_json(tank_json)
 
-    def explode_tank(self, sid):
-        self._tanks[sid].explode()
+    def explode_tank(self, tag):
+        self._tanks[tag].explode()
 
-    def delete_tank(self, sid):
-        del self._tanks[sid]
+    def delete_tank(self, tag):
+        del self._tanks[tag]
 
-    def get_tank(self, sid):
-        return self._tanks[sid]
+    def get_tank(self, tag):
+        return self._tanks[tag]
 
     def tanks_json(self):
-        return {k: self._tanks[k].to_json() for k in self._tanks}
+        return {tag: self._tanks[tag].to_json() for tag in self._tanks}
