67db84905871fecdd9585ee03628eb4deed20322
Kneasle <kneasle@gmail.com>
2020-07-09 17:44:36 +0100

    Add doc comments to tank.py

--- tank.py
+++ tank.py
@@ -1,4 +1,8 @@
+""" Module to encapsulate the data stored about a single tank. """
+
 class Tank:
+    """ A class representing a single tank stored by the server. """
+
     def __init__(self, x, y, r, col, username, sid):
         self._js_data = {
             'x': x,
@@ -16,6 +20,8 @@ class Tank:
 
 
     def respawn(self, x, y, r):
+        """ Respawn the tank by overwriting the location, and setting it to be alive. """
+
         self._js_data['isAlive'] = True
         self._js_data['x'] = x
         self._js_data['y'] = y
@@ -23,17 +29,25 @@ class Tank:
 
 
     def explode(self):
+        """ Blow up this tank. """
+
         self._js_data['isAlive'] = False
 
 
     def is_alive(self):
+        """ Returns true if this tank hasn't been blown up yet this game. """
+
         return self._js_data['isAlive']
 
 
     def update_from_json(self, json):
+        """ Update the attributes given in `json`. """
+
         for k in json:
             self._js_data[k] = json[k]
 
 
     def to_json(self):
+        """ Serialise to JSON. """
+
         return self._js_data
